02a4193bead5ce637566e383dc4f02c40606fcd9
khaled hossny <khaledhossny73@gmail.com>
2022-12-20 11:45:44 +0200

    first commit

---
+++ 01-Python_Tasks/05-Compiler_Task/test_file.cpp
@@ -0,0 +1,61 @@
+x=5;
+y=6;
+x+=5;
+y|=7;
+x*=9;
+
+x*9;
+x+9;
+x/9;
+x~x;
+x|x;
+!x;
+
+A;
+1;
+2;
+3;
+4;
+5;
+6;
+0;
+
+12;
+22000;
+Num1_;
+Num2222;
+'khaled_hossny 1'
+"Khaledhossny"
+''
+""
+/*Multiline comment*/
+x*y;
+x<y
+y!=x
+
+x||y;
+x>=y
+
+x&&y
+
+x>y;
+// this is a line comment @#$%^&*
+
+
+/*
+Multiline comment
+new comment 
+here comment
+*/
+
+/*
+Multiline comment
+new comment 
+here comment
+*/
+
+/*
+Multiline comment
+new comment 
+here comment
+*/
